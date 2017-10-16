93cc97872b107f778c3b32f9498aa491b51015d6
mccarvik <mccarviks@gmail.com>
2017-10-16 16:15:20 +0000

    making progress

--- Databases/cats.js
+++ Databases/cats.js
@@ -9,22 +9,22 @@ var catSchema = new mongoose.Schema({
 
 var Cat = mongoose.model("Cat", catSchema);
 
-//adding a new cat to the DB
+// adding a new cat to the DB
 
-// var george = new Cat({
-//     name: "Mrs. Norris",
-//     age: 7,
-//     temperament: "Evil"
-// });
+var george = new Cat({
+    name: "Mrs. Norris",
+    age: 7,
+    temperament: "Evil"
+});
 
-// george.save(function(err, cat){
-//     if(err){
-//         console.log("SOMETHING WENT WRONG!")
-//     } else {
-//         console.log("WE JUST SAVED A CAT TO THE DB:")
-//         console.log(cat);
-//     }
-// });
+george.save(function(err, cat){
+    if(err){
+        console.log("SOMETHING WENT WRONG!")
+    } else {
+        console.log("WE JUST SAVED A CAT TO THE DB:")
+        console.log(cat);
+    }
+});
 
 Cat.create({
    name: "Snow White",
---
+++ js_tricky_stuff/this.js
@@ -0,0 +1,35 @@
+// "use strict"
+
+// this is original set by default to the global object
+// which in the browser will be window
+console.log(window.this === this);
+
+
+function badIdea() {
+    // since 'badIdea' is not inside a delcared object, this will not be set
+    // mistake will be a global variable
+    this.mistake = "oops"
+}
+
+// can prevent this with "use strict" which will set 'this' to undefined by default
+
+
+// Since this is inside a defined object (person), this gets set to person
+var person = {
+    first_name: "me",
+    sayHi: function(){
+        return "Hi" + this.first_name;
+    },
+    determine_context: function(){
+        return this === person;
+    },
+    // functions wont work the same for dog as now 'this' = dog
+    dog: {
+        sayHello: function(){
+            return "Hello " + this.first_name;
+        },
+        determine_cont: function(){
+            return this === person;
+        }
+    }
+}
