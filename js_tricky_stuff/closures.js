17443da1f527565b185d80c17269cf4165818e7c
mccarvik <mccarviks@gmail.com>
2017-10-17 19:42:02 +0000

    finished couple more sections

---
+++ js_tricky_stuff/closures.js
@@ -0,0 +1,12 @@
+
+function outer(a) {
+    return function inner(b) {
+        return a + b;
+    };
+}
+
+console.log(outer(5)(5));
+var storeOuter = outer(5);
+console.log(storeOuter(10));
+
+// Closures can help establish private variables
--- js_tricky_stuff/oop.js
+++ js_tricky_stuff/oop.js
@@ -3,3 +3,42 @@ function House(bedrooms, bathrooms, sqrft) {
     this.bathrooms = bathrooms;
     this.sqrft = sqrft;
 }
+
+function dog(name, age) {
+    this.name = name;
+    this.age = age;
+    
+    this.bark = function() {
+        console.log(this.name + " just barked");
+    }
+}
+
+var rusty = new dog('Rusty', 3)
+rusty.bark()
+
+function Car(make, model, year) {
+    this.make = make;
+    this.year = year;
+    this.model = model;
+    this.numwheels = 4;
+}
+
+// 'call' / 'apply' resolves the problem of what this is referring to
+function Motorcycle(make, model, year) {
+    // Car.call(this, make, model, year);
+    Car.apply(this, arguments);
+    this.numwheel = 2;
+}
+
+function person(name) {
+    this.name = name;
+}
+
+var colt = new person('colt');
+console.log(colt.__proto__ === person.prototype);
+
+person.prototype.sayHi = function(){
+    return "Hi " + this.name;
+}
+
+console.log(colt.sayHi());
